186734e33f7c64289fa051874e001ea8dcdf6933
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2025-08-10 23:35:47 +0500

    Added Swagger API docs

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import config from './config/config';
 import database from './config/db';
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api', routes);
 
